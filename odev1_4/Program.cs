cb7d32c373497d218b1b9aa0a38fb5113fb56b58
Vahdet Savci <savcivahdet@gmail.com>
2023-10-30 13:52:52 +0300

    fourth console app done

---
+++ odev1_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace odev1_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bir cümle yazınız: ");
+        KelimeVeHarf(Console.ReadLine().Split(" "));
+    }
+
+    static void KelimeVeHarf(string[] kelimeler)
+    {
+        int n = 0;
+        foreach (string kelime in kelimeler)
+             n += kelime.Length;
+        Console.WriteLine("Girdiğiniz cümle " + kelimeler.Length + " kelime, " + n + " harf içerir.");
+    }
+}
